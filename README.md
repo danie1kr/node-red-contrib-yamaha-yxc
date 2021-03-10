6d849ee8ac1deab51cf26c738080377015f7f602
danie1kr <git.daniel@kauker.com>
2021-03-10 14:31:47 +0100

    update to documentation package version --typos

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # node-red-contrib-yamaha-yxc
 A node red module for controlling Yamaha MusicCast devices via yamaha-yxc-nodejs
+
+# Node Red details
+This project contains one node which injects the YamahaYXC into incoming messages.
+The node takes any `msg` as an input.
+The output `msg` is enriched with the `msg.yamaha` object.
+    
+This node gives you the YamahaYXC object exposing the API in the msg object as `msg.yamaha`.
+You can use this node in the following nodes, e.g. in a function node.
+To power up your Yamaha device in the main zone, just call `msg.yamaha.power("on", "main")`.
+Be aware that yamaha-yxc-nodejs returns promises, so the full function would more look like this: 
+```javascript
+msg.yamaha.power("on", "main").then(() => { 
+	msg.payload = "zone main is on"; 
+	node.send(msg); 
+}); 
+return;
+```
+
+# Referencess
+* [yamaha-yxc-nodejs](https://github.com/foxthefox/yamaha-yxc-nodejs) docs on git - full description of `msg.yamaha` property
+* [>node-red-contrib-avr-yamaha](https://github.com/krauskopf/node-red-contrib-avr-yamaha) on git - thanks to krauskopf for all the inspiration
--- yamaha-yxc.js
+++ yamaha-yxc.js
@@ -13,9 +13,6 @@ module.exports = function(RED) {
 
 		// Define functions called by nodes
 		var node = this;
-
-		this.get = function(topic) { return node.yamaha.SendGetToDevice(topic); };
-		this.post = function(topic, payload) { return node.yamaha.SendPostToDevice(topic, payload); };
 	}
 	RED.nodes.registerType("yamaha-yxc", YamahaXYCNodeConfig);
 
@@ -39,106 +36,4 @@ module.exports = function(RED) {
     	}
 	}
 	RED.nodes.registerType("yamaha-yxc-api", YamahaXYCNodeAPI);
-
-	// get
-  	function YamahaXYCNodeGet(config) {
-    	RED.nodes.createNode(this, config);
-
-	    // Save settings in local node
-	    this.device = config.device;
-	    this.deviceNode = RED.nodes.getNode(this.device);
-	    this.name = config.name;
-	    this.topic = config.topic;
-
-    	var node = this;
-    	if (this.deviceNode) {
-
-			// Input handler, called on incoming flow
-			this.on('input', function(msg) {
-	        	// If no topic is given in the config, then we use the topic in the msg.
-	        	var topic = (node.topic) ? node.topic : msg.topic;
-	    		if (!topic) {
-	          		node.error('No topic given. Specify either in the config or via msg.topic!');
-	         		return;
-	       		}
-
-	        	// Get data from the device.
-	        	node.deviceNode.get(topic).then(function(value) {
-					msg.payload = value;
-					node.send(msg);
-				}).catch(function(error) {
-	          		node.error("Failed to request data from AVR with error: " + error);       // 1 arg: send to debug output
-	          		node.error("Failed to request data from AVR with error: " + error, msg);  // 2 arg: trigger catch node
-	        	});
-	      	});
-	    } else {
-			this.error(RED._("yamaha-yxc.errors.missing-config"));
-	    }
-	}
-	RED.nodes.registerType("yamaha-yxc-get", YamahaXYCNodeGet);
-
-	// put
-	function YamahaYXCNodePut(config) {
-	    RED.nodes.createNode(this, config);
-
-	    // Save settings in local node
-	    this.device = config.device;
-	    this.deviceNode = RED.nodes.getNode(this.device);
-	    this.name = config.name;
-	    this.topic = config.topic;
-	    this.payload = config.payload;
-
-	    var node = this;
-	    if (this.deviceNode) {
-
-	  		// Input handler, called on incoming flow
-	  		this.on('input', function(msg) {
-
-		        // If no topic is given in the config, then we us the topic in the msg.
-		        var topic = (node.topic) ? node.topic : msg.topic;
-		        if (!topic) {
-		         	node.error('No topic given. Specify either in the config or via msg.topic!');
-		        	return;
-		        }
-
-				// If no payload is given in the config, then we us the payload in the msg.
-				var payload = (node.payload) ? node.payload : msg.payload;
-				if (payload === null || payload === undefined) {
-				node.error('invalid payload: ' + payload.toString());
-					return;
-				}
-
-		        /* Some commands need additional payload values. These might be given as JSON object or
-		        // automatically added.
-		        var jsonPayload = tryParseJSON(payload);
-		        if (jsonPayload !== false) {
-		          payload = jsonPayload
-		        } else {
-		          // Check if this topic needs additional payload values and add them to payload
-		          var addPayloadFormat = references.hasAdditionalPayload(topic);
-		          if (addPayloadFormat) {
-		            payload = JSON.parse(addPayloadFormat.replace('%s', payload).trim());
-		          }
-		        }*/
-
-		        // Put data to the device.
-		        node.deviceNode.post(topic, payload).then(function(value) {
-					// Continue the flow when data has been put...
-					node.send(msg);
-		        }).catch(function(error) {
-					node.error("Failed to put data to AVR with error: " + error);       // 1 arg: send to debug output
-					node.error("Failed to put data to AVR with error: " + error, msg);  // 2 arg: trigger catch node
-		        });
-	  		});
-	    } else {
-			this.error(RED._("yamaha-yxc.errors.missing-config"));
-	    }
-	}
-	RED.nodes.registerType("yamaha-yxc-put", YamahaYXCNodePut);
-
-
-  /* ---------------------------------------------------------------------------
-   * Backend informations
-   * -------------------------------------------------------------------------*/
-  // references.provideReferences(RED)
 };
